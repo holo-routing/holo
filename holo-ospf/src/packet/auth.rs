8925bac9c2145ccad34336bdb8ea2abfde8a1a31
Renato Westphal <renato@opensourcerouting.org>
2023-06-29 17:45:01 -0300

    ospf: authentication minor code cleanup
    * Add documentation comment for the HMAC_APAD static variable
    * s/md5_digest/keyed_md5_digest/ (more precise)
    * s/auth_key/key/ (more concise)
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- holo-ospf/src/packet/auth.rs
+++ holo-ospf/src/packet/auth.rs
@@ -20,6 +20,11 @@ use serde::{Deserialize, Serialize};
 use sha1::Sha1;
 use sha2::{Sha256, Sha384, Sha512};
 
+// Apad is the hexadecimal value 0x878FE1F3 repeated (L/4) times, where L is the
+// length of the hash, measured in bytes.
+//
+// The read-only Apad defined here is designed to accommodate the largest
+// supported hash length, which is 64 bytes for SHA512.
 pub static HMAC_APAD: Lazy<Vec<u8>> = Lazy::new(|| {
     [0x87, 0x8F, 0xE1, 0xF3]
         .into_iter()
@@ -43,18 +48,18 @@ pub struct AuthCtx {
 
 // ===== helper functions =====
 
-fn md5_digest(data: &[u8], auth_key: &str) -> [u8; 16] {
+fn keyed_md5_digest(data: &[u8], key: &str) -> [u8; 16] {
     // The authentication key needs to be 16-bytes long.
-    let mut auth_key = auth_key.as_bytes().to_vec();
-    auth_key.resize(16, 0);
+    let mut key = key.as_bytes().to_vec();
+    key.resize(16, 0);
 
     let mut ctx = md5::Context::new();
     ctx.consume(data);
-    ctx.consume(&auth_key);
+    ctx.consume(&key);
     *ctx.compute()
 }
 
-fn hmac_sha_digest<H>(data: &[u8], auth_key: &str) -> Vec<u8>
+fn hmac_sha_digest<H>(data: &[u8], key: &str) -> Vec<u8>
 where
     H: CoreProxy,
     H::Core: HashMarker
@@ -66,7 +71,7 @@ where
     <H::Core as BlockSizeUser>::BlockSize: IsLess<U256>,
     Le<<H::Core as BlockSizeUser>::BlockSize, U256>: NonZero,
 {
-    let mut mac = Hmac::<H>::new_from_slice(auth_key.as_bytes()).unwrap();
+    let mut mac = Hmac::<H>::new_from_slice(key.as_bytes()).unwrap();
     mac.update(data);
     mac.update(&HMAC_APAD[..H::Core::output_size()]);
     let digest = mac.finalize();
@@ -81,7 +86,7 @@ pub(crate) fn message_digest(
     key: &str,
 ) -> Vec<u8> {
     match algo {
-        CryptoAlgo::Md5 => md5_digest(data, key).to_vec(),
+        CryptoAlgo::Md5 => keyed_md5_digest(data, key).to_vec(),
         CryptoAlgo::HmacSha1 => hmac_sha_digest::<Sha1>(data, key),
         CryptoAlgo::HmacSha256 => hmac_sha_digest::<Sha256>(data, key),
         CryptoAlgo::HmacSha384 => hmac_sha_digest::<Sha384>(data, key),

8d360eb6b2fc0e2d8deddd631babe73832709107
Fréderic LOUI <frederic.loui@renater.fr>
2024-04-08 21:14:58 +0200

    move holo-cli to an external repository
    Signed-off-by: Fréderic LOUI <frederic.loui@renater.fr>

--- holo-cli/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(false)
-        .compile(&["../proto/holo.proto"], &["../proto"])?;
-    Ok(())
-}

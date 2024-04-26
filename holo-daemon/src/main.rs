a4f80cf4779ee378052a38f7340a7bb22833e7ee
Renato Westphal <renato@opensourcerouting.org>
2024-04-26 13:48:40 -0300

    daemon: fix issue with chroot jail
    The chroot jail should be disabled when the event recorder is enabled,
    so that it's possible to create files in the filesystem.
    
    Since the event recorder isn't meant to be used in production, this
    change doesn't pose a security risk.
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- holo-daemon/src/main.rs
+++ holo-daemon/src/main.rs
@@ -114,14 +114,16 @@ fn init_db<P: AsRef<Path>>(
     }
 }
 
-fn privdrop(user: &str) -> nix::Result<()> {
+fn privdrop(user: &str, chroot_jail: bool) -> nix::Result<()> {
     // Preserve set of permitted capabilities upon privdrop.
     capctl::prctl::set_securebits(capctl::prctl::Secbits::KEEP_CAPS).unwrap();
 
     // Drop to unprivileged user and group.
     if let Some(user) = User::from_name(user)? {
-        nix::unistd::chroot(&user.dir)?;
-        nix::unistd::chdir("/")?;
+        if chroot_jail {
+            nix::unistd::chroot(&user.dir)?;
+            nix::unistd::chdir("/")?;
+        }
         nix::unistd::setgroups(&[user.gid])?;
         nix::unistd::setresgid(user.gid, user.gid, user.gid)?;
         nix::unistd::setresuid(user.uid, user.uid, user.uid)?;
@@ -179,7 +181,10 @@ fn main() {
         .expect("failed to initialize non-volatile storage");
 
     // Drop privileges.
-    if let Err(error) = privdrop(&config.user) {
+    //
+    // If the event recorder is enabled, the chroot jail is disabled to allow
+    // creating files in the filesystem.
+    if let Err(error) = privdrop(&config.user, !config.event_recorder.enabled) {
         error!(%error, "failed to drop root privileges");
         std::process::exit(1);
     }

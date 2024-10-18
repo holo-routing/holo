f3bd13dd4a97a850cefd27269a1a56c0783556ad
Renato Westphal <renato@opensourcerouting.org>
2024-10-18 09:24:08 -0300

    chore: bump tonic to v0.12
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- holo-daemon/build.rs
+++ holo-daemon/build.rs
@@ -1,12 +1,14 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().build_client(false).compile(
-        &[
-            "../proto/holo.proto",
-            "../proto/gnmi_ext.proto",
-            "../proto/gnmi.proto",
-        ],
-        &["../proto"],
-    )?;
+    tonic_build::configure()
+        .build_client(false)
+        .compile_protos(
+            &[
+                "../proto/holo.proto",
+                "../proto/gnmi_ext.proto",
+                "../proto/gnmi.proto",
+            ],
+            &["../proto"],
+        )?;
 
     Ok(())
 }

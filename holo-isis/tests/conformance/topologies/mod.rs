f4105f2fa2b07b87cc35a9e61e73d62fe9a56e36
Renato Westphal <renato@opensourcerouting.org>
2025-02-21 20:17:43 -0300

    isis: add initial test topologies for conformance testing
    Add four IS-IS test topologies to lay the groundwork for upcoming
    conformance tests. These topologies include a mix of point-to-point
    and broadcast links, parallel links, IPv4 and IPv6, single-area and
    multi-area configurations, as well as both legacy and wide metrics.
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

---
+++ holo-isis/tests/conformance/topologies/mod.rs
@@ -0,0 +1,40 @@
+//
+// Copyright (c) The Holo Core Contributors
+//
+// SPDX-License-Identifier: MIT
+//
+
+use holo_isis::instance::Instance;
+use holo_protocol::test::stub::run_test_topology;
+
+#[tokio::test]
+async fn topology1_1() {
+    for rt_num in 1..=7 {
+        let rt_name = format!("rt{}", rt_num);
+        run_test_topology::<Instance>("topo1-1", &rt_name).await;
+    }
+}
+
+#[tokio::test]
+async fn topology1_2() {
+    for rt_num in 1..=7 {
+        let rt_name = format!("rt{}", rt_num);
+        run_test_topology::<Instance>("topo1-2", &rt_name).await;
+    }
+}
+
+#[tokio::test]
+async fn topology2_1() {
+    for rt_num in 1..=6 {
+        let rt_name = format!("rt{}", rt_num);
+        run_test_topology::<Instance>("topo2-1", &rt_name).await;
+    }
+}
+
+#[tokio::test]
+async fn topology2_2() {
+    for rt_num in 1..=6 {
+        let rt_name = format!("rt{}", rt_num);
+        run_test_topology::<Instance>("topo2-2", &rt_name).await;
+    }
+}

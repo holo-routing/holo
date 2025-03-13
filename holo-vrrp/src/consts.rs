a5671f846e3288dd6c52a81394bf92d4ad3e593d
Renato Westphal <renato@opensourcerouting.org>
2025-03-13 16:19:00 -0300

    addr from_str
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- holo-vrrp/src/consts.rs
+++ holo-vrrp/src/consts.rs
@@ -8,15 +8,17 @@
 //
 
 use std::net::{Ipv4Addr, Ipv6Addr};
+use std::str::FromStr;
+use std::sync::LazyLock as Lazy;
 
 pub const MAX_VIRTUAL_IP_COUNT: usize = 20;
 pub const VALID_VRRP_VERSIONS: [u8; 2] = [2, 3];
 pub const VRRP_PROTO_NUMBER: i32 = 112;
 
 // Multicast Addresses
-pub const VRRP_MULTICAST_ADDRESS_IPV4: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 18);
-pub const VRRP_MULTICAST_ADDRESS_IPV6: Ipv6Addr =
-    Ipv6Addr::new(0xff02, 0, 0, 0, 0, 0, 0, 0x12);
-
-pub const SOLICITATION_BASE_ADDRESS: Ipv6Addr =
-    Ipv6Addr::new(0xff02, 0, 0, 0, 0, 1, 0xff00, 0x00);
+pub static VRRP_MULTICAST_ADDR_IPV4: Lazy<Ipv4Addr> =
+    Lazy::new(|| Ipv4Addr::from_str("224.0.0.18").unwrap());
+pub static VRRP_MULTICAST_ADDR_IPV6: Lazy<Ipv6Addr> =
+    Lazy::new(|| Ipv6Addr::from_str("ff02::12").unwrap());
+pub static SOLICITATION_BASE_ADDR: Lazy<Ipv6Addr> =
+    Lazy::new(|| Ipv6Addr::from_str("ff02::1:ff00:0").unwrap());
